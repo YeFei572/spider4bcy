2926c8e86b3ed0d1ae36fcfa65073e33e185151b
keppel <572yefei@163.com>
2019-07-11 17:42:05 +0800

    修改一下爬虫

--- wuyixiansheng.py
+++ wuyixiansheng.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import urllib
+import time
 
 class getHtmlUrl(object):
 
@@ -8,41 +11,59 @@ class getHtmlUrl(object):
         self.server = "http://www.audio699.com/book/293/"
         self.resourceUrl = []
         self.totalPage = 2
+        self.chapter = 176
         self.path = 'd:/xmly'
 
     def getResourceUrl(self):
-        headers = {   
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'
+        headers = {         
+            'Cookie': '__cfduid=dc979bcd1056648ca6668c353d5d8cb211562156949',
+            'Host': 'www.audio699.com',
+            'Referer': 'http://www.audio699.com/book/293.html',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
         } 
         for i in range(self.totalPage):
-            req = requests.get(url=self.server + '122.html', headers=headers)
+            downloadUrl = self.server + str(i+self.chapter) + '.html'
+            print(downloadUrl)
+            req = requests.get(url=downloadUrl, headers=headers)
             bf = BeautifulSoup(req.text, "html.parser")
             audios = bf.find('source')
             self.resourceUrl.append(audios.get('src'))
 
     def downloadM4a(self):
-        no = 0
+        no = self.chapter - 1
+        print('download begin...')
         for i in self.resourceUrl:
+            print(i)
             no = no + 1
-            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}  
             try:
                 req = urllib.request.Request(i, headers=headers)
-                data = urllib.request.urlopen(req).read()
-                with open('D/XiMaLaya/' + '1.m4a', 'wb') as f:
+                res = urllib.request.urlopen(req)
+                print(res.getcode())
+                print(res.info())
+                path = r'D:\xia\\' + str(no) + '.m4a'
+                data = res.read()
+                print(path)
+                with open(path, 'wb') as f:
                     f.write(data)
                     f.close()
+                print(str(no) + '下载完毕, 下面开始沉睡10分钟.....')
+                time.sleep(100)
+                print('沉睡结束, 准备下载下一集....')
             except Exception as e:
                 print(str(e))
-            # try:
-            #     print(i)
-            #     urllib.request.urlretrieve(i, 'D:\XiMaLaya\\'+str(i+'.m4a'))
-            #     print(str(i), '...【下载成功】')
-            # except:
-            #     print(i + '下载失败了')
-
+        print('download over...')
+        # 修改总集数为原来的+原来的及数量
+        self.chapter = self.chapter + self.totalPage
+        # 清空resourceUrl为空
+        self.resourceUrl = []
 
 
 if __name__ == "__main__":
     dl = getHtmlUrl()
-    dl.getResourceUrl()
-    dl.downloadM4a()
+    for i in range(10):
+        dl.getResourceUrl()
+        dl.downloadM4a()
+        print('下一轮开始循环')
+        time.sleep(3600)
+        
